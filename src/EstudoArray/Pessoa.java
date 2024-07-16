845c8cd8e5597497af99aa97481358874eb555f2
LuizRossini <luiz.guirossini123@gmail.com>
2024-07-15 22:23:10 -0300

    creating constructor & objects

--- src/EstudoArray/Pessoa.java
+++ src/EstudoArray/Pessoa.java
@@ -13,6 +13,12 @@ public class Pessoa {
     private String nome;
     private int idade;
 
+    //Construtor
+    public Pessoa(String nome, int idade) {
+        this.nome = nome;
+        this.idade = idade;
+    }
+
     //Setters
     public void setNome(String nome) {
         this.nome = nome;
--- src/EstudoArray/Principal.java
+++ src/EstudoArray/Principal.java
@@ -5,6 +5,12 @@ public class Principal {
     public static void main(String[] args) {
         ArrayList<Pessoa> listaDePessoas = new ArrayList<>();
 
+        var pessoa01 = new Pessoa("Luiz", 20);
+        var pessoa02 = new Pessoa("Jocasta", 20);
+        var pessoa03 = new Pessoa("Lua", 13);
 
+        listaDePessoas.add(pessoa01);
+        listaDePessoas.add(pessoa02);
+        listaDePessoas.add(pessoa03);
     }
 }
